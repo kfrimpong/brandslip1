86018123f8b80403c186ce047fd407bc1dce4b0a
Joe Lipper <lipperjj@gmail.com>
2014-02-19 13:53:38 -0800

    added secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandslip::Application.config.secret_token = 'd3c99e617e1a1e17540d9c34bfd198f86774c0a60f15e2f579cd409b212348d65cca34b7e344e401c447844d308d76962d9bbbe80c0cedb14ebcee7cafeddc7d'
